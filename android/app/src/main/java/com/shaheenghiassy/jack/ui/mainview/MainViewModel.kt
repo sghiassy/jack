39602dc8a1401cb751bc964a786486adf47eaa89
Shaheen Ghiassy <sghiassy@linkedin.com>
2025-04-01 00:26:58 -0400

    Converted to StateFlow

--- android/app/src/main/java/com/shaheenghiassy/jack/ui/mainview/MainViewModel.kt
+++ android/app/src/main/java/com/shaheenghiassy/jack/ui/mainview/MainViewModel.kt
@@ -2,14 +2,17 @@ package com.shaheenghiassy.jack.ui.mainview
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
 import com.shaheenghiassy.jack.data.repositories.SimpleCounterRepository
-import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 
 class MainViewModel(application: Application): AndroidViewModel(application) {
     private val counterRepository = SimpleCounterRepository(application.applicationContext)
-    val uiState: Flow<MainViewUIState.Loaded> = counterRepository.observeCounter()
+    val uiState: StateFlow<MainViewUIState> = counterRepository.observeCounter()
         .map { ctr ->
             MainViewUIState.Loaded(ctr.increment())
-        }
+        }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(), MainViewUIState.Loading)
 }
