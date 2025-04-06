d21d97fd1e23f3ca999a9710c1ec3f21387691ae
Shaheen Ghiassy <sghiassy@linkedin.com>
2025-04-06 12:33:30 -0400

    Centered UI

--- android/app/src/main/java/com/shaheenghiassy/jack/ui/mainview/CounterText.kt
+++ android/app/src/main/java/com/shaheenghiassy/jack/ui/mainview/CounterText.kt
@@ -1,16 +1,19 @@
 package com.shaheenghiassy.jack.ui.mainview
 
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.shaheenghiassy.jack.ui.theme.JACKTheme
 
 @Composable
 fun CounterText(modifier: Modifier = Modifier, str: String) {
     Text(
         text = str,
-        modifier = modifier
+        modifier = modifier,
+        style = MaterialTheme.typography.headlineLarge.copy(fontSize = 40.sp)
     )
 }
 
