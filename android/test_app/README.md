5765695895d9567388fb7dce552e905c4b5c334e
Shaheen Ghiassy <sghiassy@linkedin.com>
2025-04-10 12:07:42 -0400

    Creating Android Test App

---
+++ android/test_app/README.md
@@ -0,0 +1,7 @@
+# Test App
+
+This is a seperate Android app used for external testing functionality for the main Jack Android app
+
+## Functionality
+
+- Send a Broadcast to the Jack app to increase its amount
---
+++ android/test_app/app/src/main/java/com/shaheenghiassy/myapplication/MainActivity.kt
@@ -0,0 +1,66 @@
+package com.shaheenghiassy.myapplication
+
+import android.R.attr.name
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.shaheenghiassy.myapplication.ui.theme.MyApplicationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyApplicationTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                        Column(
+                            horizontalAlignment = Alignment.CenterHorizontally //Center the contents horizontally
+                        ) {
+                            Greeting(
+                                name = "JACK Test App",
+                                modifier = Modifier.padding(innerPadding)
+                            )
+                            Button({
+                                Log.d("shizz", "Button Pressed")
+                            }) {
+                                Text(
+                                    text = "Send Broadcast"
+                                )
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MyApplicationTheme {
+        Greeting("Android")
+    }
+}
