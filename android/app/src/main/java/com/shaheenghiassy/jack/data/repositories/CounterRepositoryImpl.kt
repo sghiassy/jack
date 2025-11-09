39de40e097091f710964b4834b5fd4eb0c733923
Shaheen Ghiassy <sghiassy@linkedin.com>
2025-11-08 21:46:14 -0500

    Disabled Server DataSource and moving to DiskDatasource

--- android/app/src/main/java/com/shaheenghiassy/jack/data/repositories/CounterRepositoryImpl.kt
+++ android/app/src/main/java/com/shaheenghiassy/jack/data/repositories/CounterRepositoryImpl.kt
@@ -1,7 +1,7 @@
 package com.shaheenghiassy.jack.data.repositories
 
 import android.content.Context
-import com.shaheenghiassy.jack.data.datasources.APIDatasourceImpl
+import com.shaheenghiassy.jack.data.datasources.DiskDatasourceImpl
 import com.shaheenghiassy.jack.data.models.CounterModel
 import com.shaheenghiassy.jack.domain.repository.CounterRepository
 import kotlinx.coroutines.flow.Flow
@@ -10,35 +10,35 @@ import kotlinx.coroutines.flow.asSharedFlow
 
 
 class CounterRepositoryImpl(context : Context): CounterRepository {
-//    private val diskRepository = DiskDatasourceImpl(context)
-    private val apiRepository = APIDatasourceImpl()
+    private val diskRepository = DiskDatasourceImpl(context)
+//    private val apiRepository = APIDatasourceImpl()
 
     private val _myFlow = MutableSharedFlow<CounterModel>()
     override val myFlow: Flow<CounterModel> = _myFlow.asSharedFlow()
 
     override suspend fun initialize() {
-        val valueFromDisk = apiRepository.readCounter() ?: 0
+        val valueFromDisk = diskRepository.readCounter() ?: 0
         _myFlow.emit(CounterModel(valueFromDisk))
     }
 
     override suspend fun increment() {
-        val valueFromDisk = apiRepository.readCounter() ?: 0
+        val valueFromDisk = diskRepository.readCounter() ?: 0
         val newValue = valueFromDisk + 1
-        apiRepository.writeCounter(newValue)
+        diskRepository.writeCounter(newValue)
         _myFlow.emit(CounterModel(newValue))
     }
 
     override suspend fun decrement() {
-        val valueFromDisk = apiRepository.readCounter() ?: 0
+        val valueFromDisk = diskRepository.readCounter() ?: 0
         val newValue = valueFromDisk - 1
-        apiRepository.writeCounter(newValue)
+        diskRepository.writeCounter(newValue)
         _myFlow.emit(CounterModel(newValue))
     }
 
     override suspend fun change(newValue: Int) {
-        val valueFromDisk = apiRepository.readCounter() ?: 0
+        val valueFromDisk = diskRepository.readCounter() ?: 0
         val changedValue = valueFromDisk + newValue
-        apiRepository.writeCounter(changedValue)
+        diskRepository.writeCounter(changedValue)
         _myFlow.emit(CounterModel(changedValue))
     }
 }
