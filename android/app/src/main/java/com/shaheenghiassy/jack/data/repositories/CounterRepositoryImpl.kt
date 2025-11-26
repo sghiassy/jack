679d63e6e297eec3d80d4bcb15d47a4336c37bdc
Shaheen Ghiassy <sghiassy@linkedin.com>
2025-11-26 01:03:01 -0500

    updated repository for dyanmic datasource changes

--- android/app/src/main/java/com/shaheenghiassy/jack/data/repositories/CounterRepositoryImpl.kt
+++ android/app/src/main/java/com/shaheenghiassy/jack/data/repositories/CounterRepositoryImpl.kt
@@ -1,6 +1,7 @@
 package com.shaheenghiassy.jack.data.repositories
 
 import android.content.Context
+import com.shaheenghiassy.jack.data.datasources.APIDatasourceImpl
 import com.shaheenghiassy.jack.data.datasources.DiskDatasourceImpl
 import com.shaheenghiassy.jack.data.models.CounterModel
 import com.shaheenghiassy.jack.domain.repository.CounterRepository
@@ -11,34 +12,58 @@ import kotlinx.coroutines.flow.asSharedFlow
 
 class CounterRepositoryImpl(context : Context): CounterRepository {
     private val diskRepository = DiskDatasourceImpl(context)
-//    private val apiRepository = APIDatasourceImpl()
+    private val apiRepository = APIDatasourceImpl()
+
+    enum class DatasourceType {
+        DISK, API
+    }
+
+    private var activeDatasource: DatasourceType = DatasourceType.DISK
+
+    private val currentDatasource
+        get() = when (activeDatasource) {
+            DatasourceType.DISK -> diskRepository
+            DatasourceType.API -> apiRepository
+        }
+
+    fun switchToDisk() {
+        activeDatasource = DatasourceType.DISK
+    }
+
+    fun switchToAPI() {
+        activeDatasource = DatasourceType.API
+    }
+
+    fun setDatasource(type: DatasourceType) {
+        activeDatasource = type
+    }
 
     private val _myFlow = MutableSharedFlow<CounterModel>()
     override val myFlow: Flow<CounterModel> = _myFlow.asSharedFlow()
 
     override suspend fun initialize() {
-        val valueFromDisk = diskRepository.readCounter() ?: 0
+        val valueFromDisk = currentDatasource.readCounter() ?: 0
         _myFlow.emit(CounterModel(valueFromDisk))
     }
 
     override suspend fun increment() {
-        val valueFromDisk = diskRepository.readCounter() ?: 0
+        val valueFromDisk = currentDatasource.readCounter() ?: 0
         val newValue = valueFromDisk + 1
-        diskRepository.writeCounter(newValue)
+        currentDatasource.writeCounter(newValue)
         _myFlow.emit(CounterModel(newValue))
     }
 
     override suspend fun decrement() {
-        val valueFromDisk = diskRepository.readCounter() ?: 0
+        val valueFromDisk = currentDatasource.readCounter() ?: 0
         val newValue = valueFromDisk - 1
-        diskRepository.writeCounter(newValue)
+        currentDatasource.writeCounter(newValue)
         _myFlow.emit(CounterModel(newValue))
     }
 
     override suspend fun change(newValue: Int) {
-        val valueFromDisk = diskRepository.readCounter() ?: 0
+        val valueFromDisk = currentDatasource.readCounter() ?: 0
         val changedValue = valueFromDisk + newValue
-        diskRepository.writeCounter(changedValue)
+        currentDatasource.writeCounter(changedValue)
         _myFlow.emit(CounterModel(changedValue))
     }
 }
