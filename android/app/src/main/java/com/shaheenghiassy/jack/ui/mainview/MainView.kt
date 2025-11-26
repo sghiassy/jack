67d22a1b0ac3a05a65abef49b675dd3f73883740
Shaheen Ghiassy <sghiassy@linkedin.com>
2025-11-26 11:30:52 -0500

    Moved viewModel internal to MainView

--- android/app/src/main/java/com/shaheenghiassy/jack/ui/mainview/MainView.kt
+++ android/app/src/main/java/com/shaheenghiassy/jack/ui/mainview/MainView.kt
@@ -11,18 +11,21 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.shaheenghiassy.jack.ui.components.CounterText
 import com.shaheenghiassy.jack.usecase.stateToString
 
 @Composable
 fun MainView(
-    innerPadding: PaddingValues,
-    uiState: MainViewUIState,
-    viewModel: MainViewModel
+    innerPadding: PaddingValues
 ) {
+    val viewModel: MainViewModel = viewModel()
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
     Box(
         modifier = Modifier
             .fillMaxSize() // Make the Box take up the full screen
